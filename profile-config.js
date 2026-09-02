1e86ba5a0b37165bd6d20d1925d9be9240bbd86a
ipparinda-dotcom <ip.parinda@gmail.com>
2026-09-02 11:44:36 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbzZPKCjqrvptWM7nVyquVLeU2GlKrS2UtljX1vQCqDHR2UVsL_-Tyh5BqVctKUwsv1C/exec';
+  'https://script.google.com/macros/s/AKfycbw0Wd7AvK70cJCFSRc_sZ1FzI-wJlSfAQm1vKw7ikfacpae6piSO39MgAotuilr0hXi/exec';
